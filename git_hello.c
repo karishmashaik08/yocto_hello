3327b59645d2ddd112f5a3a7353ce548a84c8b32
karishmashaik08 <138648666+karishmashaik08@users.noreply.github.com>
2024-12-13 15:36:26 +0530

    Create git_hello.c

---
+++ git_hello.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+        int n;
+        printf("enter the number banner to repeat?");
+        scanf("%d",&n);
+        printf("\n");
+        for(int i=0;i<n;i++){
+                printf("***********************************************************\n");
+                printf("*****                                                 *****\n");
+                printf("*****       welcome to linux distrubution             *****\n");
+                printf("*****                                                 *****\n");
+                printf("***********************************************************\n");
+        }
+}
