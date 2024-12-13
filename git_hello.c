bcc72776c71a2b380269bf8ce4289efd78022c19
karishmashaik08 <138648666+karishmashaik08@users.noreply.github.com>
2024-12-13 17:19:47 +0530

    Update git_hello.c

--- git_hello.c
+++ git_hello.c
@@ -9,8 +9,6 @@ int main(){
         printf("*****                                                 *****\n");
         printf("***********************************************************\n");
         printf("how many times do you want me to say hello");
-
-        printf("enter the number banner to repeat?");
         scanf("%d",&n);
         printf("\n");
         for(int i=0;i<n;i++){
