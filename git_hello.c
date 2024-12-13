8bd9240157dcd92f4657741b0d02c0d0071c6a7e
karishmashaik08 <138648666+karishmashaik08@users.noreply.github.com>
2024-12-13 17:15:15 +0530

    Update git_hello.c

--- git_hello.c
+++ git_hello.c
@@ -2,14 +2,19 @@
 
 int main(){
         int n;
+      
+        printf("***********************************************************\n");
+        printf("*****                                                 *****\n");
+        printf("*****       hello welcome to QEMU screen              *****\n");
+        printf("*****                                                 *****\n");
+        printf("***********************************************************\n");
+        printf("how many times do you want me to say hello");
+
         printf("enter the number banner to repeat?");
         scanf("%d",&n);
         printf("\n");
         for(int i=0;i<n;i++){
-                printf("***********************************************************\n");
-                printf("*****                                                 *****\n");
-                printf("*****       welcome to linux distrubution             *****\n");
-                printf("*****                                                 *****\n");
-                printf("***********************************************************\n");
+                printf("hello\n");
+                
         }
 }
